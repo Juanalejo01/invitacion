4232b8726e1475d7301f4993c9f3c01edfff8fac
Juan <juanangeloti771@gmail.com>
2024-12-02 02:08:15 +0100

    Añadir Formulario.css

--- src/components/Temporizador.jsx
+++ src/components/Temporizador.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Temporizador.css";
+import "./temporizador.css";
 
 const Temporizador = ({ targetDate }) => {
   const [timeLeft, setTimeLeft] = useState({
